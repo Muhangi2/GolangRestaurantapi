37828c897e26900d38bda2981045552f194ee4ed
Muhangi2 <eliodamutiba@gmail.com>
2024-02-09 23:35:32 +0300

    itemsorder

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -2,17 +2,21 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"golang-Restaurantbooking/database"
 	"golang-Restaurantbooking/models"
+	"log"
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type orderItem struct {
+type neworderItem struct {
 	Table_id    *string
 	Order_items []models.OrderItem
 }
@@ -63,34 +67,85 @@ func GetOrderItem() gin.HandlerFunc {
 }
 func CreateOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
-     var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-	 var orderItem models.OrderItem
-	  if err:=  c.BindJSON(&orderItem);
-	  err != nil {
-		  c.JSON(400, gin.H{"error": err.Error()})
-	  }
-	  //validate the orderItem
-	  validateError := validate.Struct(orderItem)
-    if validateError != nil {
-		c.JSON(400, gin.H{"error": validateError.Error()})
-	}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var orderItem models.OrderItem
+		if err := c.BindJSON(&orderItem); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+		}
+		var orderitemStructure neworderItem
+		var orders models.Order
+		orders.Order_Date = time.Now()
+		orders.Table_id = orderitemStructure.Table_id
+		orderItemsTobeCreated := []interface{}{}
+		order_id := OrderItemOrderCreator(order)
 
-	  orderItem.Created_at = time.Now()
-	  orderItem.Updated_at = time.Now()
-	  orderItem.ID = primitive.NewObjectID()
-	  orderItem.Order_item_id = orderItem.ID.Hex()
-	  result, insertErr := orderItemCollection.InsertOne(ctx, orderItem)
-      	  if insertErr != nil {
-					  c.JSON(500, gin.H{"error": "Error while creating the orderItem"})
-		  }
-		  defer cancel()
-		  c.JSON(201, orderItem)
+		for _, orderItem := range orderitemStructure.Order_items {
+			orderItem.Order_id = order_id
+			validateError := validate.Struct(orderItem)
+			if validateError != nil {
+				c.JSON(400, gin.H{"error": validateError.Error()})
+			}
+
+			orderItem.ID = primitive.NewObjectID()
+			orderItem.Order_item_id = orderItem.ID.Hex()
+			orderItem.Created_at = time.Now()
+			orderItem.Updated_at = time.Now()
+			var num = toFixed(*orderItem.Unit_price, 2)
+			orderItem.Unit_price = &num
+			orderItemsTobeCreated = append(orderItemsTobeCreated, orderItem)
+
+		}
+
+		result, insertErr := orderItemCollection.InsertMany(ctx, orderItemsTobeCreated)
+
+		if insertErr != nil {
+			log.Fatal(insertErr)
+			c.JSON(500, gin.H{"error": "Error while creating the orderItem"})
+		}
+		defer cancel()
+		c.JSON(201, result)
 
 	}
 }
 func UpdateOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		var orderItem models.OrderItem
+		if err := c.BindJSON(&orderItem); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error})
+			return
+		}
+		orderItemId := c.Param("orderItem_id")
+		filter := bson.M{"order_item_id": orderItemId}
 
+		var updateObj primitive.D
+		if orderItem.Unit_price != nil {
+			updateObj = append(updateObj, bson.E{"unit_price": *orderItem.Unit_price})
+		}
+		if orderItem.Quantity != nil {
+			updateObj = append(updateObj, bson.E{"quantity": *orderItem.Quantity})
+		}
+		if orderItem.Food_id != nil {
+			updateObj = append(updateObj, bson.E{"food_id": *orderItem.Food_id})
+		}
+		orderItem.Updated_at = time.Now()
+		updateObj = append(updateObj, bson.E{"updated_at": orderItem.Updated_at})
+
+		//finalizing with inserting in the mongoDb
+		upsert := true
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+		result, err := orderCollection.UpdateOne(
+			ctx, filter, bson.D{{"$set", updateObj}}, &opt,
+		)
+		if err != nil {
+			msg := fmt.Sprintf("orderitem updated ")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		defer cancel()
+		c.JSON(201, result)
 	}
 }
 func DeleteOrderItem() gin.HandlerFunc {
--- models/orderModel.go
+++ models/orderModel.go
@@ -9,7 +9,7 @@ import (
 type Order struct {
 	ID         primitive.ObjectID `json:"_id,omitempty" `
 	Order_id   string             `json:"order_id" `
-	Table_id   string             `json:"table_id" validate:"required"`
+	Table_id   *string            `json:"table_id" validate:"required"`
 	Order_Date time.Time          `json:"order_date" validate:"required" `
 	Created_at time.Time          `json:"created_at" `
 	Updated_at time.Time          `json:"updated_at" `
