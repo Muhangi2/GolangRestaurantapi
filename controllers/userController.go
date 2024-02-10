d63ef5e059660e5c85af153141f930d1effb2591
Muhangi2 <eliodamutiba@gmail.com>
2024-02-10 17:46:38 +0300

    users

--- controllers/userController.go
+++ controllers/userController.go
@@ -2,28 +2,94 @@ package controllers
 
 import (
 	"context"
+	"golang-Restaurantbooking/database"
+	"golang-Restaurantbooking/models"
+	"log"
+	"net/http"
 	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 
 func GetUsers() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-		recordperpage,err:=strconv.Atoi(c.Query("recordperpage"))
+		recordperpage, err := strconv.Atoi(c.Query("recordperpage"))
+		if err != nil || recordperpage < 1 {
+			recordperpage = 10
+		}
+		page, err := strconv.Atoi(c.Query("page"))
+		if err != nil || page < 1 {
+			page = 1
+		}
+		startIndex := (page - 1) * recordperpage
+		startIndex, err = strconv.Atoi(c.Query("startIndex"))
+
+		matchStage := bson.D{{"$match", bson.D{{}}}}
+		projectStage := bson.D{{"$project", bson.D{
+			{"_id", 0},
+			{"total_count", 1},
+			{"user_items", bson.D{
+				{"$slice", []interface{}{"$data", startIndex, recordperpage}},
+			}},
+		}}}
+
+		result, err := userCollection.Aggregate(ctx, mongo.Pipeline{
+			matchStage, projectStage,
+		})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing to user items"})
+		}
+		var allUsers []bson.M
+		if err = result.All(ctx, &allUsers); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allUsers)
+
 	}
 
 }
 func GetUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-		   
+		userId := c.Param("User_id")
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId})
+		defer cancel()
+		if err != nil {
+			c.JSON(500, gin.H{"error": "error occured while fetching the user"})
+		}
+		c.JSON(200, user)
 	}
 }
 func Signup() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		//change to struct form
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+		//validate the struct
+		validationErr := validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+		}
+		//check for email
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while checking the email"})
+			return
+		}
+		
 
 	}
 }
